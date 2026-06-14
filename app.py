6ce95907618f16f8992bbc8fdb6c7386a0ae8011
adam-j-curtis <adamjcurtis@gmail.com>
2026-06-14 10:58:59 -0400

    Set up pathlib and project retrieval

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask
+from pathlib import Path
+
+PROJECTS_DIR = Path("projects")
+PROJECTS_DIR.mkdir(exist_ok=True)
+
+print(PROJECTS_DIR.resolve())
 
 app = Flask(__name__)
 
+def get_projects() :
+    projects = []
+    for item in PROJECTS_DIR.iterdir() :
+        if item.is_dir() :
+            projects.append(item.name)
+    return projects
+
 @app.route("/")
 def home() :
-    return "<h1>The Cap Safe</h1>"
+    projects = get_projects()
+    return f"""
+    <h1>The Cap Safe</h1>
+    <p>{projects}</p>
+    """
 
 if __name__ == "__main__" :
     app.run(debug=True)
+
