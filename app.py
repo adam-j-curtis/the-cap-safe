2580b755ad1da7dfaebaf275119701ed5018829b
adam-j-curtis <adamjcurtis@gmail.com>
2026-06-16 12:51:50 -0400

    Getting images to display in subcategories

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from pathlib import Path
 
 PROJECTS_DIR = Path("projects")
@@ -7,6 +7,8 @@ PROJECTS_DIR.mkdir(exist_ok=True)
 print(PROJECTS_DIR.resolve())
 
 app = Flask(__name__)
+
+# Helpers
 
 def get_projects() :
     projects = []
@@ -62,53 +64,67 @@ def get_images(project_name, category, subcategory) :
 
 def clickable_images(project_name, category, subcategory) :
     images = get_images(project_name, category, subcategory)
-    return f"""
-    <p>{images}</p>
-    """
+    string = ""
+    for image in images :
+        string += f"""
+            <li>
+                <img src="/files/{project_name}/{category}/{subcategory}/{image}" width="200">
+                <p>{image}</p>
+            </li>
+            """
+    return string
+
+# Routes
+
+@app.route("/files/<path:filename>")
+def files(filename) :
+    return send_from_directory(PROJECTS_DIR, filename)
 
 @app.route("/")
 def home() :
     projects = get_projects()
     return f"""
-    <h1>The Cap Safe</h1>
-    <ul>
-        {clickable_projects()}
-    </ul>
-    """
+        <h1>The Cap Safe</h1>
+        <ul>
+            {clickable_projects()}
+        </ul>
+        """
 
 @app.route("/projects/<project_name>")
 def view_project(project_name) :
     return f"""
-    <h1>{project_name}</h1>
-    <h2>Categories</h2>
-    <ul>
-        {clickable_categories(project_name)}
-    </ul>
-    <p><a href="/">Home</a></p>
-    """
+        <h1>{project_name}</h1>
+        <h2>Categories</h2>
+        <ul>
+            {clickable_categories(project_name)}
+        </ul>
+        <p><a href="/">Home</a></p>
+        """
 
 @app.route("/projects/<project_name>/<category>")
 def view_category(project_name, category) :
     return f"""
-    <h1>{project_name} / {category}</h1>
-    <h2>Subcategories</h2>
-    <ul>
-        {clickable_subcategories(project_name, category)}
-    </ul>
-    <p><a href="/projects/{project_name}">Back to project</a></p>
-    <p><a href="/">Home</a></p>
-    """
+        <h1>{project_name} / {category}</h1>
+        <h2>Subcategories</h2>
+        <ul>
+            {clickable_subcategories(project_name, category)}
+        </ul>
+        <p><a href="/projects/{project_name}">Back to project</a></p>
+        <p><a href="/">Home</a></p>
+        """
 
 @app.route("/projects/<project_name>/<category>/<subcategory>")
 def view_subcategory(project_name, category, subcategory) :
     return f"""
-    <h1>{project_name} / {category} / {subcategory}</h1>
-    <ul>
-        {clickable_images(project_name, category, subcategory)}
-    </ul>
-    <p><a href="/projects/{project_name}/{category}">Back to category</a></p>
-    <p><a href="/">Home</a></p>
-    """
+        <h1>{project_name} / {category} / {subcategory}</h1>
+        <ul>
+            {clickable_images(project_name, category, subcategory)}
+        </ul>
+        <p><a href="/projects/{project_name}/{category}">Back to category</a></p>
+        <p><a href="/">Home</a></p>
+        """
+
+# Run
 
 if __name__ == "__main__" :
     app.run(debug=True)
